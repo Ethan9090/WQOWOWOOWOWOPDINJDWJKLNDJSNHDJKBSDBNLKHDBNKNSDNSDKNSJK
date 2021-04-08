e9035752a79a7016f3c1b16201b1eeeaa122b4a3
Ethan9090 <71732564+Ethan9090@users.noreply.github.com>
2021-04-08 18:40:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
     var Msound1 = "";
     var Msound2 = "";
 
+    var leftsoundhandy = 0;
+    var leftsoundhandx = 0;
+
+    var rightsoundhandy = 0;
+    var rightsoundhandx = 0;
+
 function preload(){
 
     Msound1 = loadSound("mysong.mp3");
@@ -18,11 +24,31 @@ function setup(){
     camera.hide();
 
     MyModel = ml5.poseNet(camera,modelLoaded);
+
+    MyModel.on('pose', gotposes);
     
 }
 
 function modelLoaded(){
     console.log("Posenet works now");
+    
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        
+        console.log(results);
+    
+        leftsoundhandy = results[0].pose.rightWrist.y;
+        leftsoundhandx = results[0].pose.rightWrist.x;
+
+        rightsoundhandy = results[0].pose.rightWrist.y;
+        rightsoundhandx = results[0].pose.rightWrist.x;
+
+        console.log("left hand x is " + leftsoundhandx + " left handy is " + leftsoundhandy);
+        console.log("right hand x is " + rightsoundhandx + " right handy is " + rightsoundhandy);
+
+    }
 }
 
 function draw(){
